ba352778536f1427edf409a31c8d2c694cb760d8
lianli <lianlitongji2002@163.com>
2020-04-05 22:18:16 +0800

    support toggle nodes

--- src/components/AwesomeTree.vue
+++ src/components/AwesomeTree.vue
@@ -13,41 +13,101 @@ export default {
       type: Number,
       default: 0,
     },
+    isParentLastNode: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: function () {
+    return {
+      cache: {},
+    };
   },
   components: {
     AwesomeTreeNode,
   },
+  methods: {
+    onToggle: function (key) {
+      if (this.cache[key].expand === undefined) {
+        this.$set(this.cache[key], "expand", true);
+      } else {
+        this.cache[key].expand = !this.cache[key].expand;
+      }
+    },
+  },
   render() {
     return (
-      <div>
-        {this.nodes.map((node) => {
+      <ul class={!this.isParentLastNode && this.level ? "line-conn tree" : "tree"}>
+        {this.nodes.map((node, index) => {
           if (node.children && node.children.length > 0) {
+            this.cache[node.key] = node;
             return (
               <ul>
-                <awesome-tree-node key={node.key} node={node} level={this.level + 1} />
-                <awesome-tree nodes={node.children} level={this.level + 1} />
+                <awesome-tree-node
+                  key={node.key}
+                  node={node}
+                  level={this.level + 1}
+                  isFirst={index === 0}
+                  isLast={index === this.nodes.length - 1}
+                  isExpand={node.expand}
+                  isLeaf={!node.children || node.children.length === 0}
+                  vOn:toggle={this.onToggle}
+                />
+                {node.expand && (
+                  <awesome-tree
+                    nodes={node.children}
+                    level={this.level + 1}
+                    isParentLastNode={index === this.nodes.length - 1}
+                  />
+                )}
               </ul>
             );
           } else {
             return (
-              <ul>
-                <awesome-tree-node key={node.key} node={node} level={this.level + 1} />
-              </ul>
+              <awesome-tree-node
+                key={node.key}
+                node={node}
+                level={this.level + 1}
+                isFirst={index === 0}
+                isLast={index === this.nodes.length - 1}
+                isExpand={node.expand}
+                isLeaf={!node.children || node.children.length === 0}
+                vOn:toggle={this.onToggle}
+              />
             );
           }
         })}
-      </div>
+      </ul>
     );
   },
 };
 </script>
 <style scoped>
-ul {
+.tree,
+.tree ul,
+.tree li {
   list-style-type: none;
+  font-size: 12px;
   margin-block-start: 0;
   margin-block-end: 0;
   margin-inline-start: 0;
   margin-inline-end: 0;
   padding-inline-start: 0;
+  display: block;
+  max-height: 1000px;
+  overflow: hidden;
+}
+.tree > li {
+  padding-left: 18px;
+}
+.tree > ul {
+  padding-left: 18px;
+}
+.tree li {
+  display: flex;
+}
+.line-conn {
+  background-image: url(/line_conn.gif);
+  background-repeat: repeat-y;
 }
 </style>
--- src/components/AwesomeTreeNode.vue
+++ src/components/AwesomeTreeNode.vue
@@ -1,12 +1,22 @@
 <template>
   <li>
-    <div :style="{ display: 'inline-block', width: offset * (level - 1) + 'px' }"></div>
+    <div
+      class="indicator"
+      @click="$emit('toggle', node.key)"
+      :class="computeIndicatorClass"
+      :style="{
+        display: 'inline-block',
+        height: indicatorWidth + 'px',
+        width: indicatorWidth + 'px',
+      }"
+    />
     {{ node.label }}
   </li>
 </template>
 
 <script>
-const OFFSET = 20;
+const OFFSET = 18;
+const INDICATOR_WIDTH = 18;
 export default {
   props: {
     node: {
@@ -19,11 +29,116 @@ export default {
       type: Number,
       default: 0,
     },
+    isFirst: {
+      type: Boolean,
+      default: false,
+    },
+    isLast: {
+      type: Boolean,
+      default: false,
+    },
+    isLeaf: {
+      type: Boolean,
+      default: true,
+    },
+    isExpand: {
+      type: Boolean,
+      default: false,
+    },
+    toggle: {
+      type: Function,
+    },
   },
   data: function () {
     return {
       offset: OFFSET,
+      indicatorWidth: INDICATOR_WIDTH,
     };
+  },
+  computed: {
+    computeIndicatorClass: function () {
+      if (this.isFirst && this.isLast && this.isExpand && !this.isLeaf) {
+        return "unique-expand";
+      }
+      if (this.isFirst && this.isLast && !this.isExpand && !this.isLeaf) {
+        return "unique-collapse";
+      }
+      if (this.isFirst && this.isExpand && !this.isLeaf) {
+        return "first-expand";
+      }
+      if (this.isFirst && !this.isExpand && !this.isLeaf) {
+        return "first-collapse";
+      }
+      if (this.isLast && this.isExpand && !this.isLeaf) {
+        return "last-expand";
+      }
+      if (this.isLast && !this.isExpand && !this.isLeaf) {
+        return "last-collapse";
+      }
+      if (!this.isFirst && !this.isLast && this.isExpand && !this.isLeaf) {
+        return "middle-expand";
+      }
+      if (!this.isFirst && !this.isLast && !this.isExpand && !this.isLeaf) {
+        return "middle-collapse";
+      }
+      if (this.isFirst && this.isLast && this.isLeaf) {
+        return "unique-leaf";
+      }
+      if (this.isFirst && this.isLeaf) {
+        return "first-leaf";
+      }
+      if (this.isLast && this.isLeaf) {
+        return "last-leaf";
+      }
+      if (!this.isFirst && !this.isLast && this.isLeaf) {
+        return "middle-leaf";
+      }
+      return "";
+    },
   },
 };
 </script>
+<style>
+.indicator {
+  background-image: url("/zTreeStandard.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+.indicator:hover {
+  cursor: pointer;
+  opacity: 0.8;
+}
+.unique-expand {
+  background-position: -92px -54px;
+}
+.unique-collapse {
+  background-position: -74px -54px;
+}
+.first-expand {
+  background-position: -92px 0;
+}
+.first-collapse {
+  background-position: -74px 0;
+}
+.last-expand {
+  background-position: -92px -36px;
+}
+.last-collapse {
+  background-position: -74px -36px;
+}
+.middle-expand {
+  background-position: -92px -18px;
+}
+.middle-collapse {
+  background-position: -74px -18px;
+}
+.first-leaf {
+  background-position: -56px -18px;
+}
+.last-leaf {
+  background-position: -56px -36px;
+}
+.middle-leaf {
+  background-position: -56px -18px;
+}
+</style>
